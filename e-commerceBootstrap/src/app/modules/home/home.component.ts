763842e72474db21c947e67a36db56cbfc1e2758
Thaíssa Durães <thaissaduraes67@gmail.com>
2022-09-14 13:19:00 -0300

    add rotas home, ultilizando componente card-product

--- e-commerceBootstrap/src/app/modules/home/home.component.ts
+++ e-commerceBootstrap/src/app/modules/home/home.component.ts
@@ -23,40 +23,40 @@ export class HomeComponent implements OnInit {
 
   products = [
     {
-      id: 1, vl_product: '00.00', srcImg: 'assets/products/praia/praia1.jpg',name_product: 'Camisa 1'
+      id: 1, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia1.jpg',name_product: 'Roupa de Banho 1'
     },
     {
-      id: 2, vl_product: '00.00', srcImg: 'assets/products/praia/praia4.jpg', name_product: 'Camisa 2'
+      id: 2, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia4.jpg', name_product: 'Roupa de Banho 2'
     },
     {
-      id: 3, vl_product: '00.00', srcImg: 'assets/products/praia/praia4.jpg', name_product: 'Camisa 3'
+      id: 3, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia5.jpg', name_product: 'Roupa de Banho 3'
     },
     {
-      id: 4, vl_product: '70.00', srcImg: 'assets/products/praia/praia5.jpg', name_product: 'Camisa 4'
+      id: 4, vl_product: 'R$ 70.00', srcImg: 'assets/products/praia/praia6.jpg', name_product: 'Roupa de Banho 4'
     },
     {
-      id: 5, vl_product: '22300.00', srcImg: 'assets/products/praia/praia5.jpg', name_product: 'Camisa 5'
+      id: 5, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia7.jpg', name_product: 'Roupa de Banho 5'
     },
     {
-      id: 6, vl_product: '32.00', srcImg: 'assets/products/praia/praia5.jpg', name_product: 'Camisa 6'
+      id: 6, vl_product: 'R$ 32.00', srcImg: 'assets/products/praia/praia8.jpg', name_product: 'Roupa de Banho 6'
     },
     {
-      id: 7, vl_product: '1222.00', srcImg: 'assets/products/praia/praia5.jpg', name_product: 'Camisa 7'
+      id: 7, vl_product: 'R$ 22.00', srcImg: 'assets/products/praia/praia9.jpg', name_product: 'Roupa de Banho 7'
     },
     {
-      id: 8, vl_product: '00.00', srcImg: 'assets/products/praia/praia1.jpg', name_product: 'Camisa 8'
+      id: 8, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia11.jpg', name_product: 'Roupa de Banho 8'
     },
     {
-      id: 9, vl_product: '00.00', srcImg: 'assets/products/praia/praia1.png', name_product: 'Camisa 9'
+      id: 9, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia4.jpg', name_product: 'Roupa de Banho 9'
     },
     {
-      id: 10, vl_product: '00.00', srcImg: 'assets/products/praia/praia1.png', name_product: 'Camisa 10'
+      id: 10, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia5.jpg', name_product: 'Roupa de Banho 10'
     },
     {
-      id: 11, srcImg: 'assets/products/praia/praia1.png', name_product: 'Camisa 11'
+      id: 11, vl_product: 'R$ 70,00', srcImg: 'assets/products/praia/praia6.jpg', name_product: 'Roupa de Banho 11'
     },
     {
-      id: 12, srcImg: 'assets/products/praia/praia1.png', name_product: 'Camisa 12'
+      id: 12, vl_product: 'R$ 54,90', srcImg: 'assets/products/praia/praia7.jpg', name_product: 'Roupa de Banho 12'
     }
   ]
 
