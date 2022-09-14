83022be712957181f18cacbb1355d3cd41f97e62
Thaíssa Durães <thaissaduraes67@gmail.com>
2022-09-14 16:55:11 -0300

    list-product

--- e-commerceBootstrap/src/app/modules/products/list-products/list-products.component.ts
+++ e-commerceBootstrap/src/app/modules/products/list-products/list-products.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent implements OnInit {
-
+  products=[]
   constructor() { }
 
   ngOnInit() {
